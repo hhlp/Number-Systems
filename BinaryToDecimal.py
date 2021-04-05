1d0fe6987af20b5a032c11609629a9eac0d3e66d
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-05 17:36:12 +0200

    Update BinaryToDecimal.py

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -1,13 +1,22 @@
 class BinaryConverter(object):
-    def __init__(self, byte_string="11111111.11111111.11111111.11111111"):
-        self.byte_string = byte_string
+    def __init__(self, bit_string="11111111.11111111.11111111.11111111"):
+        self.bit_string = bit_string
+        self.byte_list = bit_string.split(".")
         
     def __str__(self):
-        return str(self.byte_string)
+        return str(f"{self.bit_string}\n{self.convertion()}")
     
     def __repr__(self):
         return str("Takes a bit string, splits it into bytes and converts it to decimal notation.")
     
+    def convertion(self):
+        decimal_notation = ""
+        for bit in self.byte_list:
+            decimal_notation += str(int(bit, 2)) + "."
+        last_dot = decimal_notation[-1]
+        return decimal_notation.strip(last_dot)
+    
+
 if __name__ == "__main__":
     test = BinaryConverter()
     print(test)
