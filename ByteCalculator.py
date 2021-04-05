fa60c2f342b1565285c84da0a3f58778bf4a3cc3
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-05 17:15:13 +0200

    Create ByteCalculator.py

---
+++ ByteCalculator.py
@@ -0,0 +1,13 @@
+class ByteCalc(object):
+    def __init__(self, byte_string="11111111.11111111.11111111.11111111"):
+        self.byte_string = byte_string
+        
+    def __str__(self):
+        return str(self.byte_string)
+    
+    def __repr__(self):
+        return str("Takes a bit string, splits it into bytes and converts it to decimal notation.")
+    
+if __name__ == "__main__":
+    test = ByteCalc()
+    print(test)
