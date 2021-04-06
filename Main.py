27404238ee1818527261111595fe5989206f53b8
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-06 09:13:05 +0200

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 from BinaryToDecimal import BinaryToDecimal
 from DecimalToBinary import DecimalToBinary
@@ -12,32 +13,44 @@ base_menu = str("""
 [2] Hexadecimal.
 """)
 
+
 def base_selection():
     bases = {
-        "0": sys.exit(),
-        "1": binary(),
-        "2": hexadecimal()
+        "0": sys.exit,
+        "1": binary,
+        "2": hexadecimal
     }
     print(base_menu)
+    print("[ Base Selection ]:")
     try:
         choice = str(int(input("  >> ")))
         if -1 > choice > 2:
             print("Please enter a number between 0 and 2") 
     except TypeError:
         print("Please enter a number")
-    return bases[choice]
+    method = bases[choice]
+    return method()
 
 
 def binary():
-    print("Binary selected")
+    print("\n\n[ Binary Convertion ]")
+    print("Please enter your bit-string or number sequence:")
+    convert_me = input("  >> ")
+    for num in convert_me:
+        if num in str(range(2, 9)):
+            print("Decimal Input!")
+            # print(DecimalToBinary(convert_me))
+        else:
+            print("Binary Input!")
+            print(BinaryToDecimal(convert_me))
 
 
 def hexadecimal():
-    print("Hexadecimal selected")
-
-base_selection()
+    print("[ Hexadecimal Convertion ]")
+    print("Please enter your hexadecimal-string or number sequence:")
+    convert_me = input("  >> ")
 
 
 if __name__ == "__main__":
-    print(base_selection())
+    base_selection()
     
