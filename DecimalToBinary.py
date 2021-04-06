4b95e82e0caa1f3d0ea8dba8e4c68f6762d3aabc
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-06 07:49:26 +0200

    Create DecimalToBinary.py

---
+++ DecimalToBinary.py
@@ -0,0 +1,29 @@
+class BinaryToDecimal(object):
+    def __init__(self, num_seq="255.255.255.255"):
+        self.num_seq = num_seq
+        self.num_list = num_seq.split(".")
+        
+        
+    def __str__(self):
+        return str(self.convertion())
+    
+    
+    def __repr__(self):
+        return repr("This object takes in a number sequence, splits it into parts, and converts it into binary notation.")
+    
+    
+    def convertion(self):
+        binary_notation = ""
+        for num in self.num_list:
+            binary_notation += str("{0:08b}.".format(int(num)))
+        last_dot = binary_notation[-1]
+        return binary_notation.strip(last_dot)
+    
+
+if __name__ == "__main__":
+    num_seq = BinaryToDecimal("127.192.168.1")
+    print(f"Passed bit-string:  {num_seq}\n"
+          f"Default bit-string: {BinaryToDecimal()}\n"
+          f"Object description: {repr(BinaryToDecimal())}")
+
+
