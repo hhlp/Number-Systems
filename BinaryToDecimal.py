89a6b3e9ebd2ba90ecfedbff252100421438f9e0
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-06 07:58:07 +0200

    hexadecimal

--- BinaryToDecimal.py
+++ BinaryToDecimal.py
@@ -25,5 +25,3 @@ if __name__ == "__main__":
     print(f"Passed bit-string:  {bit_string}\n"
           f"Default bit-string: {BinaryToDecimal()}\n"
           f"Object description: {repr(BinaryToDecimal())}")
-
-
--- DecimalToHex.py
+++ DecimalToHex.py
@@ -1,4 +1,4 @@
-class DecimalToBinary(object):
+class DecimalToHex(object):
     def __init__(self, num_seq="255.255.255.255"):
         self.num_seq = num_seq
         self.num_list = num_seq.split(".")
@@ -9,19 +9,19 @@ class DecimalToBinary(object):
     
     
     def __repr__(self):
-        return repr("This object takes in a number sequence, splits it into parts, and converts it into binary notation.")
+        return repr("This object takes in a number sequence, splits it into parts, and converts it into hexadecimal notation.")
     
     
     def convertion(self):
         binary_notation = ""
         for num in self.num_list:
-            binary_notation += str("{0:08b}.".format(int(num)))
+            binary_notation += str("{0:x}.".format(int(num)))
         last_dot = binary_notation[-1]
         return binary_notation.strip(last_dot)
     
 
 if __name__ == "__main__":
-    num_seq = DecimalToBinary("127.192.168.1")
+    num_seq = DecimalToHex("127.192.168.1")
     print(f"Passed bit-string:  {num_seq}\n"
-          f"Default bit-string: {DecimalToBinary()}\n"
-          f"Object description: {repr(DecimalToBinary())}")
+          f"Default bit-string: {DecimalToHex()}\n"
+          f"Object description: {repr(DecimalToHex())}")
